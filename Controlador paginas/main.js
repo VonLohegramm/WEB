6aca8cab84fd2203c2ee6c4ab121bae1d6b332f0
VonLohegramm <35115169+VonLohegramm@users.noreply.github.com>
2019-01-07 19:52:06 -0200

    Add files via upload

---
+++ Controlador paginas/main.js
@@ -0,0 +1,41 @@
+function mudar(qtd)
+{
+	//Pega a URL atual
+	var url = document.location.href;
+	//Pega a posição onde se encontra a substring 'page-'
+	var aux = url.indexOf("page-");
+
+	if(aux == -1)
+	{
+		url = url + "page-1";
+		var aux = url.indexOf("page-");
+	}
+
+	//Pegamos apenas a substring 'page-' + '?'(? é o numero)
+	var aux2 = url.substring(aux, url.lenght);
+
+	//Encontramos o numero da pagina
+	var num = aux2.replace(/[^0-9]/g, '');
+	//Converte a string para inteiro
+	num  = parseInt(num);
+	num = num + qtd;
+
+	//Aqui criamos a string da proxima paina
+	var stringFinal = url.replace(aux2, "page-" + num);
+
+	//Pegamos o texto do botão 'Proximo'
+	var a = document.getElementsByClassName('pageNav-jump pageNav-jump--next')[0];
+
+	//Caso exista exista o botão, pularemos para proxima pagina
+	if(a.innerText != "")
+	{
+		window.location = stringFinal;
+	}
+}
+
+document.querySelector('body').addEventListener('keydown', function(event){
+		if(event.keyCode == 39)
+			mudar(1);
+		if(event.keyCode == 37)
+			mudar(-1);
+	});
